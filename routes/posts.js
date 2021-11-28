850ab1bc4bd5834e80700b469cd72ec86984903d
vikash <vikashkumar754189@gmail.com>
2021-11-29 03:03:28 +0530

    no any user without sign in can post

--- routes/posts.js
+++ routes/posts.js
@@ -4,6 +4,6 @@ const passport = require("passport");
 
 const postsController = require("../controllers/posts_controller");
 
-router.post("/create", passport.checkAuthentication, postsController.create);
+router.post("/create", passport.checkAuthentication, postsController.create); //checking whether user is sign in or not
 
 module.exports = router;
