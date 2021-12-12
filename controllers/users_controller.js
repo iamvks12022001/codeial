967f90673e2aa7b5961c58fdbffdc078569d770b
vikash <vikashkumar754189@gmail.com>
2021-12-12 20:28:18 +0530

    fixing some issues

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,6 @@
 const User = require("../models/user");
 
 module.exports.profile = function (req, res) {
-  //finding the user's profile by id
   User.findById(req.params.id, function (err, user) {
     return res.render("profile", {
       title: "User Profile",
@@ -10,13 +9,8 @@ module.exports.profile = function (req, res) {
   });
 };
 
-//so that user can update the profile info
-
 module.exports.update = function (req, res) {
   if (req.user.id == req.params.id) {
-    // User.findByIdAndUpdate(req.params,id,{name:req.body.name,email:req.body.email},function(err,user){
-
-    // });
     User.findByIdAndUpdate(req.params.id, req.body, function (err, user) {
       return res.redirect("/");
     });
@@ -27,7 +21,7 @@ module.exports.update = function (req, res) {
 
 module.exports.signUp = function (req, res) {
   if (req.isAuthenticated()) {
-    return res.redirect("/users/profile");
+    return res.redirect("/users/profile" + req.user.id); // fixing the profile link issues
   }
   return res.render("user_sign_up", {
     title: "iCoder | Sign Up",
@@ -36,7 +30,7 @@ module.exports.signUp = function (req, res) {
 
 module.exports.signIn = function (req, res) {
   if (req.isAuthenticated()) {
-    return res.redirect("/users/profile");
+    return res.redirect("/users/profile" + req.user.id);
   }
 
   return res.render("user_sign_in", {
@@ -70,11 +64,11 @@ module.exports.create = function (req, res) {
 // sign in that is create the session
 
 module.exports.createSesion = function (req, res) {
-  return res.redirect("/"); //to redirect to home page
+  return res.redirect("/users/profile/" + req.user.id); //to redirect to profile page
 };
 
 module.exports.destroySession = function (req, res) {
-  req.logout(); //to delete the  cookies and logout// it is passport functionality
+  req.logout();
 
   return res.redirect("/");
 };
