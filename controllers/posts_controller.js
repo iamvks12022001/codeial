4287d05c0e23f8154a91353cb28d8b6c25c65d08
vikash <vikashkumar754189@gmail.com>
2021-12-16 22:18:20 +0530

    sending the post as ajax req to local

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -3,10 +3,20 @@ const Comment = require("../models/comment");
 
 module.exports.create = async function (req, res) {
   try {
-    await Post.create({
+    let post = await Post.create({
       content: req.body.content,
       user: req.user._id,
     });
+    //it will get the post from home_poage.js from js file
+    //and send to the local
+    if (req.xhr) {
+      return res.status(200).json({
+        data: {
+          post: post,
+        },
+        message: "post created !",
+      });
+    }
     req.flash("success", "Post published");
     return res.redirect("back");
   } catch (error) {
