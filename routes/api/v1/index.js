6de4e23c1371a6395a782b89e6a1ac57299717d0
vikash <vikashkumar754189@gmail.com>
2021-12-25 18:32:13 +0530

    add api directory  for version 1

---
+++ routes/api/v1/index.js
@@ -0,0 +1,8 @@
+//routes of all things in version 1 of api
+//ex.. api/v1/post,  api/v1/comments etc
+
+const express = require("express");
+const router = express.Router();
+
+router.use("/posts", require("./posts"));
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,8 @@ router.get("/", homeController.home);
 router.use("/users", require("./users"));
 router.use("/posts", require("./posts")); //for every post related command it go to posts route
 
+//router for api
+router.use("/api", require("./api"));
+
 router.use("/comments", require("./comments"));
 module.exports = router;
