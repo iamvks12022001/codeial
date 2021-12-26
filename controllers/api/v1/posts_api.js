5f3e28a18361a0d392a57155a39a1e1d0f9d39f9
vikash <vikashkumar754189@gmail.com>
2021-12-26 13:59:45 +0530

    sending data from DB via api and deletethe post by api call

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,39 @@
-module.exports.index = function (req, res) {
+//to show all post from database
+const Post = require("../../../models/post");
+module.exports.index = async function (req, res) {
+  let posts = await Post.find({})
+    .sort("-createdAt") //sorting based on time of creatino of post....later created apper first
+    .populate("user")
+    .populate({
+      path: "comments",
+      populate: {
+        path: "user",
+      },
+    });
   return res.json(200, {
     message: "List of Posts",
-    posts: [],
+    posts: posts,
   });
+};
+
+//to delete the post via api call
+const Comment = require("../../../models/comment");
+module.exports.destroy = async function (req, res) {
+  try {
+    let post = await Post.findById(req.params.id);
+
+    post.remove();
+
+    await Comment.deleteMany({ post: req.params.id });
+    //it will get the post from home_poage.js from js file
+    //and send to the local
+
+    return res.json(200, {
+      message: "post and assosiated comment deleted succesfully !",
+    });
+  } catch (error) {
+    return res.json(500, {
+      message: "internal server error",
+    });
+  }
 };
--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -5,4 +5,5 @@ const postsApi = require("../../../controllers/api/v1/posts_api");
 
 router.get("/", postsApi.index);
 
+router.delete("/:id", postsApi.destroy); //if post id is passed then delete  that post(localhost:8000/api/v2/posts/456666)
 module.exports = router;
