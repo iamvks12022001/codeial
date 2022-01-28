d6c752ca429d7c24d0174d84c38407523f5e0ce5
vikash <vikashkumar754189@gmail.com>
2022-01-28 21:29:00 +0530

    get comment id but error on comment name ,change some frontend as well

--- models/comment.js
+++ models/comment.js
@@ -11,6 +11,7 @@ const commentSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
+
     post: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Post",
