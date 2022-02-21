c5a38c30a951f0b227ef8c3b3bc99e2898a2d609
vikash <vikashkumar754189@gmail.com>
2022-02-21 16:27:13 +0530

    new start

--- routes/api/v1/index.js
+++
@@ -1,9 +0,0 @@
-//routes of all things in version 1 of api
-//ex.. api/v1/post,  api/v1/comments etc
-
-const express = require("express");
-const router = express.Router();
-
-router.use("/posts", require("./posts"));
-router.use("/users", require("./users"));
-module.exports = router;
